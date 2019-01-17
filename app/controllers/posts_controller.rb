556188c49a91ce7f4697619901debbb24f1c596a
Grace Shih <vocalise@gmail.com>
2019-01-17 05:37:49 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post!, only: [:show, :edit, :update]
 
   def show
-    @post = Post.find(params[:id])
+    set_post!
   end
 
   def edit
