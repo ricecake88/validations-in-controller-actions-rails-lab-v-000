a3b20c5226fffd53fbd70ed3afcd6a64ae0d0bd3
Grace Shih <vocalise@gmail.com>
2019-01-17 05:44:45 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validates :category, presence: true, inclusion: {in: %w("Fiction", "Non-Fiction")}
+  validates :category, inclusion: {in: %w("Fiction", "Non-Fiction")}
   validates :content, presence: true, length: {minimum: 100}
 end
